df0c37221c52a14a43459c612526dee17e460357
Edson Ribeiro Junior <Ribeirojj035@gmail.com>
2025-03-10 19:04:38 -0300

    Implementations in forms e modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { useState} from "react";
+import Modal from "react-modal"
 import { TransactionTable } from "./components/TransactionTable";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
+Modal.setAppElement("#root")
 //import styled from './index'
 export function App() {
+  const [isNewTransctionModalOpen, setIsNewTransctionModalOpen] = useState(false);
+
+  function handleOpenNewTransctionModal(){
+    setIsNewTransctionModalOpen(true);
+  }
+  function handleCloseNewTransctionModal(){
+    setIsNewTransctionModalOpen(false);
+  }
   return (
     <>
-     <Header/>
+     <Header onOpenNewTransactionModal={handleOpenNewTransctionModal}/>
      <Dashboard/>
      <TransactionTable/>
+     <NewTransactionModal 
+      isOpen={isNewTransctionModalOpen}
+      onRequestClose={handleCloseNewTransctionModal}
+     />
      <GlobalStyle/>
     </>
   );
